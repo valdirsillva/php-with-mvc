2195ed561f6d0cf894a605395fa11f907c196869
Valdir Silva <valdipiresba@gmail.com>
2022-07-24 19:30:10 -0300

    criando primeira classe model

--- app/Controller/Pages/Home.php
+++ app/Controller/Pages/Home.php
@@ -2,15 +2,23 @@
 
 namespace App\Controller\Pages;
 
+
 use App\Utils\View;
+
+use App\Model\Entity\Organization;
+
 
 class Home extends Page
 {
     public static function getHome() 
     {
+        $organization = new Organization;
+        
+
         $content = View::render('pages/home', [
-            "name" => "Valdir - Software Enginee",
-            "description" => "course PHP & MVC"
+            "name" => $organization->name,
+            "description" => $organization->description,
+            "site" => $organization->site
         ]);
 
         return parent::getPage('Pagina teste', $content);
---
+++ app/Model/Entity/Organization.php
@@ -0,0 +1,20 @@
+<?php 
+
+
+
+namespace App\Model\Entity;
+
+
+class Organization 
+{
+    public int $id = 1;
+
+    public string $name = "Gocakes Confeitaria";
+
+    public string $site = "http://gocakesconfeitaria.com.br";
+
+    public string $description = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+    Consequuntur facere tenetur iste nulla natus nobis nisi, reiciendis aspernatur! Debitis dolor vitae a, 
+    laborum soluta animi saepe vero ex dolore assumenda";
+
+}
